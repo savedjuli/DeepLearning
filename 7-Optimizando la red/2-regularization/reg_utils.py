97c355a41d0d682758acb86432503f7900c3f4d0
igomez666 <47692407+igomez666@users.noreply.github.com>
2025-03-04 14:30:12 +0100

    Cambios 4/03/2025

--- 7-Optimizando la red/2-regularization/reg_utils.py
+++ 7-Optimizando la red/2-regularization/reg_utils.py
@@ -309,13 +309,13 @@ def load_planar_dataset(randomness, seed):
 
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
-    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
-    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     h = 0.01
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     # Predict the function value for the whole grid
-    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
